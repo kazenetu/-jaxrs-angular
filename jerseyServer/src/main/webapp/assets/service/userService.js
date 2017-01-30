277c190f1123d56b8ea38b01b05cefd9a219df12
kazenetu <rakudafactory@gmail.com>
2017-01-30 20:50:28 +0900

    ブラウザでリロードしてもユーザーIDをクリアしないように対応

--- jerseyServer/src/main/webapp/assets/service/userService.js
+++ jerseyServer/src/main/webapp/assets/service/userService.js
@@ -4,10 +4,15 @@ myApp.service('userService', function() {
 
   this.setName = function(name){
     this.name = name;
+    sessionStorage.setItem("userName",this.name);
   };
 
   this.getName = function(){
+
     if(this.name === ""){
+      this.name = sessionStorage.getItem("userName");
+    }
+    if(this.name === null || this.name===""){
       return "ななし";
     }
     return this.name;
